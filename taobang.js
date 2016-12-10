d96ca89f0b9267f9047838d658549b539e9219fd
botuongda <b0tu0ngda@yahoo.com.vn>
2016-12-10 20:44:52 +0700

    Add files via upload

---
+++ taobang.js
@@ -0,0 +1,110 @@
+var sp1 = 
+	{
+	ten:"Bình nóng lạnh Ariston 20 lít",
+	masp:"R20", 
+	giakm:"2,550,000 đ", 
+	info:"Thiết bị ổn định nhiệt giữ vai trò rất quan trọng đối với máy nước nóng, với 2 chức năng chính: bộ cảm biến thứ nhất sẽ dừng quá trình làm nóng khi nước đã đạt đến nhiệt độ mong muốn, bộ cảm biến thứ hai sẽ tự động ngắt nguồn điện trong trường hợp nhiệt quá cao để bảo đảm an toàn cho người sử dụng.",
+	dungtich:"20 lít",
+	soluong: "100",
+	baohanh:"7 năm",
+	anh:"<img src=" + 'images\\ariston\\Binh-nong-lanh-ariston-andris-lux.jpg' + ">",
+	}; 
+
+var sp2 = 
+	{
+	ten:"Bình nóng lạnh Ariston 15L ANDRIS R 15 lít",
+	masp:"R15", 
+	giakm:"2,250,000 đ", 
+	info:"Thanh đốt bằng đồng bền vượt trội: Đồng là kim loại đặc biệt với ưu điểm kéo dài tuổi thọ của thanh đốt: tăng hơn 50% so với thanh đốt bằng thép không gỉ thông thường. Nhờ sử dụng 100% nguyên liệu an toàn – không mạ kền, có tác dụng kháng khuẩn – thanh đốt bằng đồng đem lại nguồn nước trong lành, tốt hơn cho sức khỏe người sử dụng.",
+	dungtich:"15 lít",
+	soluong:"15",
+	baohanh:"7 năm",
+	anh:"<img src="+'images\\ariston\\Binh-nong-lanh-ariston-andris-R.jpg'+" alt="+'Bình nóng lạnh Ariston 15L ANDRIS R 15 lít'+">",
+	}; 
+
+var sp3 = 
+	{
+	ten:"Bình nóng lạnh Ariston 30L ANDRIS RS 30 lít",
+	masp:"RS30", 
+	giakm:"2,960,000 đ", 
+	info:"Thiết bị ổn định nhiệt giữ vai trò rất quan trọng đối với máy nước nóng, với 2 chức năng chính: bộ cảm biến thứ nhất sẽ dừng quá trình làm nóng khi nước đã đạt đến nhiệt độ mong muốn, bộ cảm biến thứ hai sẽ tự động ngắt nguồn điện trong trường hợp nhiệt quá cao để bảo đảm an toàn cho người sử dụng.",
+	dungtich:"30 lít",
+	soluong:"5",
+	baohanh:"7 năm",
+	anh:"<img src=" + 'images\\ariston\\Binh-nong-lanh-ariston-andris-R.jpg' + ">",
+	}; 
+
+var sp4 = 
+	{
+	ten:"Bình nóng lạnh Ariston ANDRIS LUX 30 lít",
+	masp:"LUX", 
+	giakm:"3,250,000 đ", 
+	info:"Lấy cảm hứng từ đường nét uốn lượn gọn gàng, mềm mại của chiếc xe đua thể thao và của những công trình kiến trúc Ý danh tiếng, thiết kế UP đem đến sức sống mới cho dòng sản phẩm máy nước nóng Ariston với “Đường cong Latinh” khơi gợi cảm giác gia đình gần gũi và mang dấu ấn tương lai. Hòa quyện trong đường cong tuyệt hảo, Andris LUX với màu sắc đen trắng đem lại cảm quan sang trọng cho phòng tắm của bạn.",
+	dungtich:"30 lít",
+	soluong:"11",
+	baohanh:"7 năm",
+	anh:"<img src=" + 'images\\ariston\\Binh-nong-lanh-ariston-andris-lux.jpg' + ">",
+	}; 
+var sp5 = 
+	{
+	ten:"Bình nóng lạnh Ariston 30L SLIM 30 lít",
+	masp:"SLIM30QH", 
+	giakm:"3,250,000 đ", 
+	info:"Là sự kết hợp hoàn hảo giữa thiết kế và công nghệ, SL30 SLIM đem đến tiện nghi đỉnh cao cho khách hàng khi trang bị đèn báo kép màu xanh lần lượt hiển thị thời điểm nước nóng sẵn sàng ở mỗi bình chứa. Đèn số 1 sẽ sáng lên báo hiệu nước ở bình chứa 15 lít đầu tiên đã  được làm nóng đến nhiệt độ bạn mong muốn. Lúc này, bạn có hai lựa chọn: 1. Tắt thiết bị và tận hưởng làn nước ấm nóng, 2. Nếu cần dùng nhiều nước nóng hơn, bạn tiếp tục chờ đến khi đèn số 2 báo hiệu toàn bộ dung tích trong bình đã được làm nóng. Lần đầu tiên giới thiệu đến người tiêu dùng, công nghệ đèn báo kép của Ariston là giải pháp giúp bạn tiết kiệm năng lượng và thời gian vô cùng hiệu quả.",
+	dungtich:"30 lít",
+	soluong:"52",
+	baohanh:"7 năm",
+	anh:"<img src=" + 'images\\ariston\\Binh-nong-lanh-ariston-slim-20bnew.jpg' + ">",
+	}; 
+var sp6 = 
+	{
+	ten:"Bình nóng lạnh Ariston S20",
+	masp:"S20", 
+	giakm:"2,450,000 đ", 
+	info:"S20 với thiết kế nằm ngang giúp thu hẹp không gian lắp đặt của máy đồng thời hài hòa với thiết kế phòng tắm của bạn.Thanh đốt tự làm sạch được làm từ hợp kim thép không rỉ đặc biệt, được xử lý bề mặt (sáng bóng). Nhờ đó mà quá trình trao đổi nhiệt với nước hiệu quả hơn và giúp việc tiêu tốn năng lượng thấp và tăng tuổi thọ bình chứa.",
+	dungtich:"20 lít",
+	soluong:"42",
+	baohanh:"7 năm",
+	anh:"<img src=" + 'images\\ariston\\S201.jpg' + ">",
+	}; 
+var sp7 = 
+	{
+	ten:"Bình nóng lạnh Ariston ANDRIS L 30 lít",
+	masp:"L30", 
+	giakm:"3,250,000 đ", 
+	info:"Lấy cảm hứng từ đường nét uốn lượn gọn gàng, mềm mại của chiếc xe đua thể thao và của những công trình kiến trúc Ý danh tiếng, thiết kế UP đem đến sức sống mới cho dòng sản phẩm máy nước nóng Ariston với “Đường cong Latinh” khơi gợi cảm giác gia đình gần gũi và mang dấu ấn tương lai. Hòa quyện trong đường cong tuyệt hảo, Andris LUX với màu sắc đen trắng đem lại cảm quan sang trọng cho phòng tắm của bạn.",
+	dungtich:"30 lít",
+	soluong:"68",
+	baohanh:"7 năm",
+	anh:"<img src=" + 'images\\ariston\\Binh-nong-lanh-ariston-andris-lux.jpg' + ">",
+	}; 
+
+
+var array=([sp1,sp2,sp3,sp4,sp5,sp6,sp7]);
+var	arrayid=(["sp1","sp2","sp3","sp4","sp5","sp6","sp7"])
+
+
+
+
+
+for (var i = 0; i < array.length; i++) {
+		var str="";
+		var id="";
+		id=arrayid[i];
+		id= "#" + id
+		console.log(id)
+		str += "<td>"+ array[i].ten +"</td>"
+		str += "<td>"+ array[i].masp +"</td>"
+		str += "<td>"+ array[i].giakm +"</td>"
+		str += "<td>"+ array[i].info +"</td>"
+		str += "<td>"+ array[i].dungtich +"</td>"
+		str += "<td>"+ array[i].soluong +"</td>"
+		str += "<td>"+ array[i].baohanh +"</td>"
+		str += "<td>"+ array[i].anh +"</td>"
+		$(id).html(str);
+		console.log(str)
+
+}
+$(document).ready(function(){
+    $('#myTable').DataTable();
+});
